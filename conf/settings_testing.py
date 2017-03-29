86accb92383487b7c5b0897445de058d75310977
guomaoqiu <guomaoqiu@gmail.com>
2017-03-29 19:27:43 +0800

    change testing

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': '123.com',                        # 你的数据库password
-        'HOST': '192.168.1.129',                   		   # 数据库HOST
+        'HOST': '192.168.56.129',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
